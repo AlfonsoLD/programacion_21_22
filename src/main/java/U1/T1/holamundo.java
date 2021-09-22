f69ded925f887f2179478ac073eabf787d30be20
fon <alum.alopezd.2021@iesalixar.org>
2021-09-22 17:13:15 +0200

    version3

--- src/main/java/U1/T1/holamundo.java
+++ src/main/java/U1/T1/holamundo.java
@@ -3,5 +3,6 @@ package U1.T1;
 public class holamundo {
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        System.out.println("adios mundo");
     }
 }
