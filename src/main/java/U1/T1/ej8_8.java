d10888db27da76de8e01ee7150e8d2d9b91bcb21
AlfonsoLD <alum.alopezd.2021@iesalixar.org>
2021-10-15 21:02:20 +0200

    prueba pa windows

--- src/main/java/U1/T1/ej8_8.java
+++ src/main/java/U1/T1/ej8_8.java
@@ -5,15 +5,14 @@ import java.util.Scanner;
 public class ej8_8 {
     public static void main(String[] args) {
         Scanner teclado = new Scanner(System.in);
-        int resultado;
+        int resultado = 0;
 
         System.out.println("pon un numero");
         int n = teclado.nextInt();
-        for (int i = 1; i <= n; i++) {
-            for (int j = n - 1; j < n; j++) ;
-            {resultado = n * (n - 1);
+        for (int i = 0; i <= n; i++) {
+            for (int j = n - 1; j <= n; j++)
+            resultado = n * j;
             }
             System.out.println("el factorial de" + n + "es" + resultado);
         }
     }
-}
